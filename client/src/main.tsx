51b0dc63d63d476aefbd6aa129ddfe1963717481
this-is-neha <nehushah12@gmail.com>
2025-04-29 17:32:43 +0545

    nehatmkl

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,38 +1,76 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css"
+// import "@fortawesome/fontawesome-free/css/all.css"
+// import { BrowserRouter } from "react-router-dom";
+// import RouterConfig from "./config/router.config";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
+// import store,{persistor} from "./config/store.config"
+// import { LoadingComponent } from "./components/common";
+// import { AuthProvider } from "./context/auth.context";
+// import {BrandProvider} from "./context/brand-context";
+// import  {CartProvider}  from "./context/cart.context"
+// const htmlRoot: HTMLElement=document.getElementById('root') as HTMLElement;
+
+// const RootElement=ReactDOM.createRoot(htmlRoot)
+
+// RootElement.render(
+//   <React.StrictMode>
+
+//    <BrandProvider>
+ 
+//    <CartProvider>
+//    <Provider store ={store}>
+//     <PersistGate loading={<LoadingComponent/>} persistor={persistor}>
+//    <BrowserRouter>
+
+//   <AuthProvider> <RouterConfig/> </AuthProvider>
+ 
+  
+//    </BrowserRouter>
+//    </PersistGate>
+//    </Provider></CartProvider>
+ 
+//    </BrandProvider>
+   
+// </React.StrictMode>
+// )
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"
-import "@fortawesome/fontawesome-free/css/all.css"
+import "./index.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import { BrowserRouter } from "react-router-dom";
 import RouterConfig from "./config/router.config";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import store,{persistor} from "./config/store.config"
+import store, { persistor } from "./config/store.config";
 import { LoadingComponent } from "./components/common";
 import { AuthProvider } from "./context/auth.context";
-import {BrandProvider} from "./context/brand-context";
-import  {CartProvider}  from "./context/cart.context"
-const htmlRoot: HTMLElement=document.getElementById('root') as HTMLElement;
+import { BrandProvider } from "./context/brand-context";
+import { CartProvider } from "./context/cart.context";
 
-const RootElement=ReactDOM.createRoot(htmlRoot)
+const htmlRoot: HTMLElement = document.getElementById('root') as HTMLElement;
+
+const RootElement = ReactDOM.createRoot(htmlRoot);
 
 RootElement.render(
   <React.StrictMode>
-
-   <BrandProvider>
- 
-   <CartProvider>
-   <Provider store ={store}>
-    <PersistGate loading={<LoadingComponent/>} persistor={persistor}>
-   <BrowserRouter>
-
-  <AuthProvider> <RouterConfig/> </AuthProvider>
- 
-  
-   </BrowserRouter>
-   </PersistGate>
-   </Provider></CartProvider>
- 
-   </BrandProvider>
-   
-</React.StrictMode>
-)
+    <BrandProvider>
+      <CartProvider>
+        <Provider store={store}>
+          {/* PersistGate wrapping the store and routes */}
+          <PersistGate loading={<LoadingComponent />} persistor={persistor}>
+            <BrowserRouter>
+              <AuthProvider>
+                <RouterConfig />
+              </AuthProvider>
+            </BrowserRouter>
+          </PersistGate>
+        </Provider>
+      </CartProvider>
+    </BrandProvider>
+  </React.StrictMode>
+);
