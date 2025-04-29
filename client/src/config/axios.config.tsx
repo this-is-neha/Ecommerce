e40cfd67ac568a01f322d5e3ceeec472f58b0067
this-is-neha <nehushah12@gmail.com>
2025-04-29 16:43:31 +0545

    nehatmly

--- client/src/config/axios.config.tsx
+++ client/src/config/axios.config.tsx
@@ -1,24 +1,61 @@
-import axios from "axios";
-const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_API_URL,
-    timeout:30000,
-    timeoutErrorMessage:"Server timed out",
-    headers:{
-        "Content-Type":"application/json"
-    }
-})
-axiosInstance.interceptors.response.use((response)=>{
+// import axios from "axios";
+// const axiosInstance = axios.create({
+//     baseURL:import.meta.env.VITE_API_URL,
+//     timeout:30000,
+//     timeoutErrorMessage:"Server timed out",
+//     headers:{
+//         "Content-Type":"application/json"
+//     }
+// })
+// axiosInstance.interceptors.response.use((response)=>{
 
-return response.data
-},(exception)=>{
-    if(exception.code==='ERR_BAD_REQUEST'){
-        throw exception.response.data
-    }
-    else{
-        console.log("ResponseIntecep:",exception)
-        throw exception
+// return response.data
+// },(exception)=>{
+//     if(exception.code==='ERR_BAD_REQUEST'){
+//         throw exception.response.data
+//     }
+//     else{
+//         console.log("ResponseIntecep:",exception)
+//         throw exception
     
-    }
+//     }
     
-})
-export default axiosInstance
+// })
+// export default axiosInstance
+
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 30000,
+  timeoutErrorMessage: "Server timed out",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// ✅ Add this interceptor to include token
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Already present: handling response or error
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (exception) => {
+    if (exception.code === "ERR_BAD_REQUEST") {
+      throw exception.response.data;
+    } else {
+      console.log("ResponseIntercep:", exception);
+      throw exception;
+    }
+  }
+);
+
+export default axiosInstance;
